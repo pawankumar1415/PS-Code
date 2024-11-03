25b995da10dfcb051ac13b273a8e0a401edea72b
Pritam Kushwaha <pritam.kushwaha@bsbiconsulting.co.uk>
2024-11-03 21:44:18 +0530

    change: updated asn_source_based_consolidation.py

--- Python/asn_source_based_consolidation.py
+++ Python/asn_source_based_consolidation.py
@@ -12,6 +12,14 @@ source_priority = {'PDB': 1, 'IXPDB': 2, 'HE': 3, 'RIR': 4}
 # Add a temporary column for sorting by source priority
 df['source_priority'] = df['Source'].map(source_priority)
 
+# Populate 'Organization_Name' into 'IX_Name' where source is 'IXPDB'
+df.loc[df['Source'] == 'IXPDB', 'IX_Name'] = df['Organization_Name']
+
+# Ensure ID fields have no decimal places by converting them to integers
+id_fields = ['Organization_Id', 'IX_ID', 'Network_ID']
+for field in id_fields:
+    df[field] = df[field].fillna(0).astype(int)
+
 # Drop duplicate rows based on the entire dataframe
 df = df.drop_duplicates()
 
@@ -27,4 +35,4 @@ df_sorted = df_sorted.drop(columns=['source_priority'])
 # Save the sorted dataframe to a CSV file
 df_sorted.to_csv('Filtered_ASN_Consolidated_Data_file.csv', index=False)
 
-print("Dataframe sorted, duplicates removed (overall and based on ASN), and saved to 'sorted_file.csv'")
+print("Dataframe sorted, duplicates removed (overall and based on ASN), and saved to 'Filtered_ASN_Consolidated_Data_file.csv'")
